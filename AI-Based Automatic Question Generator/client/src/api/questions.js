1a8d340749354cddade0198c94ed2e4235f0e29b
Hemant M B <hemantmbadagiriyannanaver@gmail.com>
2026-05-24 13:30:39 +0530

    connected frontend to backend

--- AI-Based Automatic Question Generator/client/src/api/questions.js
+++ AI-Based Automatic Question Generator/client/src/api/questions.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
-
+const API_BASE = "https://ai-based-automatic-question-generator.onrender.com/";
 export const generateQuestions = async (params) => {
   const response = await axios.post(`${API_BASE}/generate`, params);
   return response.data;
